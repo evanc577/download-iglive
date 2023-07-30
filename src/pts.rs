4fec00d220dae1fba704c3ca1a05fd2f5bc820b6
Evan Chang <evanc577@gmail.com>
2023-07-30 17:23:59 -0400

    Add pts.rs

---
+++ src/pts.rs
@@ -0,0 +1,34 @@
+use std::process::Stdio;
+
+use anyhow::Result;
+use tokio::{process::Command, io::AsyncWriteExt};
+
+pub async fn get_pts(data: Vec<u8>) -> Result<(usize, usize)> {
+    let mut child = Command::new("ffprobe")
+        .args([
+            "-v",
+            "0",
+            "-show_entries",
+            "stream=start_pts,duration_ts",
+            "-of",
+            "compact=p=0:nk=1",
+            "-",
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let mut stdin = child.stdin.take().unwrap();
+    tokio::spawn(async move {
+        stdin.write_all(&data).await.unwrap();
+    });
+    let output = child.wait_with_output().await.unwrap();
+    if !output.status.success() {
+        panic!("ffmpeg error");
+    }
+    let data = String::from_utf8(output.stdout).unwrap();
+    let data = data.split_once('|').unwrap();
+    let pts_start = data.0.trim().parse().unwrap();
+    let pts_end = data.1.trim().parse().unwrap();
+    Ok((pts_start, pts_end))
+}
